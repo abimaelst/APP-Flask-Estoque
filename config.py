9972b73c65537b3384778b8af8c6e0225171ca16
abimaelst <abimaelst@live.com>
2020-07-31 00:07:31 -0300

    initial config of project

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+import random
+import string
+
+
+class Config(object):
+    CSRF_ENABLE = True
+    SECRET = 'ysb_92=qe#djf8%ng+a*#4rt#5%3*4k5%i2bck*gn@w3@f&-&'
+    TEMPLATE_FOLDER = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'templates')
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    APP = None
+
+
+class DevelopmentConfig(Config):
+    TESTING = True
+    DEBUG = True
+    IP_HOST = 'localhost'
+    PORT_HOST = '8000'
+    URL_MAIN = 'http://%s:%s' % (IP_HOST, PORT_HOST)
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    IP_HOST = 'localhost'  # ip do servidor na nuvem (geralmente)
+    PORT_HOST = 5000
+    URL_MAIN = 'http://%s:%s' % (IP_HOST, PORT_HOST)
+
+
+class ProductionConfig(Config):
+    TESTING = False
+    DEBUG = False
+    IP_HOST = 'localhost'  # Ip do servidor de produção
+    PORT_HOST = 8080
+    URL_MAIN = 'http://%s:%s' % (IP_HOST, PORT_HOST)
+
+
+app_config = {
+    'development': DevelopmentConfig(),
+    'testing': TestingConfig(),
+    'production': ProductionConfig(),
+}
+
+app_active = os.getenv('FLASK_ENV')
